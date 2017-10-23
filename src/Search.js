b06c745792eba5d9a32e2f820d6898de80422dac
Tyler Hervey <hervey711@gmail.com>
2017-10-23 17:13:49 +0800

    initial commit
    created seperate components for the three shelves and the search page.
    Added search functionality to search component form.

---
+++ src/Search.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+
+
+class Search extends Component {
+constructor(props) {
+super(props);
+this.state= {
+  query: '',
+  books: []
+}
+}
+
+  _updateQuery = (query) => {
+    this.setState({ query: query.trim()})
+
+    if (query !== '' || query !== undefined) {
+      BooksAPI.search(this.state.query, 20).then((books) => {
+        this.setState({books})
+      }
+
+)
+}
+}
+  render() {
+
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className="close-search" to="/">Close</Link>
+          <div className="search-books-input-wrapper">
+            {/*
+              NOTES: The search from BooksAPI is limited to a particular set of search terms.
+              You can find these search terms here:
+              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+              you don't find a specific author or title. Every search is limited by search terms.
+            */}
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={(event) => this._updateQuery(event.target.value)}
+            />
+          </div>
+        </div>
+        <div className="search-books-results">
+          {console.log(this.state.query)}
+          <ol className="books-grid">
+            {this.state.books !== undefined &&( this.state.books.map((book) => (
+            <li key={book.id}>
+              <div className="book">
+                <div className="book-top">
+                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                  <div className="book-shelf-changer">
+                    <select>
+                      <option value="none" disabled>Move to...</option>
+                      <option value="currentlyReading">Currently Reading</option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors}</div>
+              </div>
+            </li>
+          ))
+        )
+    }
+        </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Search
