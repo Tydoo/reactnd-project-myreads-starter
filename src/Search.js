f70bc05d7c3940c0584043292e4ccac1522b2c08
Tyler Hervey <hervey711@gmail.com>
2017-10-25 16:57:05 +0800

    book component
    Passed state from search down into Books props.

--- src/Search.js
+++ src/Search.js
@@ -26,7 +26,6 @@ this.state= {
 }
 
   render() {
-
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -49,32 +48,9 @@ this.state= {
           </div>
         </div>
         <div className="search-books-results">
-          {console.log(this.state.query)}
-          <ol className="books-grid">
-            {this.state.results !== undefined && (this.state.results.map((result) => (
-            <li key={result.id}>
-              {console.log(result)}
-              <Book books={this.state.results}/>
-              {/* <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${result.imageLinks.thumbnail})` }}></div>
-                  <div className="book-shelf-changer">
-                    <select>
-                      <option value="none" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="book-title">{result.title}</div>
-                <div className="book-authors">{result.authors}</div>
-              </div> */}
-            </li>
-          )))
-    }
-        </ol>
+          <ol className="books-grid" >
+            <Book results= {this.state.results}/>
+            </ol>
         </div>
       </div>
     )
--- src/book.js
+++ src/book.js
@@ -2,28 +2,41 @@ import React, {Component} from 'react'
 
 
 class Book extends React.Component {
-
+constructor(props) {
+  super(props);
+}
   render () {
-    console.log(this.props.books[0].title)
+    if (this.props.books === undefined || this.props.books === '') {
+      Book = null;
+    }
+
+    console.log(this.props.books)
     return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.books/*[]*/.imageLinks.thumbnail})` }}></div>
-          <div className="book-shelf-changer">
-            <select>
-              <option value="none" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
+      <li>
+        {(this.props.books.map((book,i) => (
+          <div className="book">
+            {console.log(this.props.books[i])}
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.book[i].imageLinks.thumbnail})` }}></div>
+            <div className="book-shelf-changer">
+              <select>
+                <option value="none" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
           </div>
+          <div className="book-title">{this.props.books.title}</div>
+          <div className="book-authors">{this.props.books.authors}</div>
         </div>
-        <div className="book-title">{this.props.books.title}</div>
-        <div className="book-authors">{this.props.books.authors}</div>
-      </div>
+      )
     )
-  }
+    )}
+  </li>
+  )
+}
 }
 
 export default Book
