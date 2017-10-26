56102b55af8dabb6dd6dbff8ade4420233243d49
Tyler Hervey <hervey711@gmail.com>
2017-10-26 12:09:26 +0800

    restructured project
    troubleshooting BookGrid. props undefined after mount.

--- src/Search.js
+++ src/Search.js
@@ -1,32 +1,10 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
-import * as BooksAPI from './BooksAPI'
-import Book from './Book'
 
 class Search extends Component {
-constructor(props) {
-super(props);
-this.state= {
-  query: '',
-  results: []
-}
-}
-
-  _updateQuery = (query) => {
-    this.setState({ query: query.trim()})
-    this._searchBooks(query)
-      }
-
-  _searchBooks = (query) => {
-    if (query !== '') {
-      BooksAPI.search(this.state.query, 20).then(
-        (results) => { this.setState( {results}) })
-      }
-      return
-}
-
   render() {
-    return (
+
+    return(
       <div className="search-books">
         <div className="search-books-bar">
           <Link className="close-search" to="/">Close</Link>
@@ -39,22 +17,36 @@ this.state= {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input
-              type="text"
-              placeholder="Search by title or author"
-              value={this.state.query}
-              onChange={(event) => this._updateQuery(event.target.value)}
-            />
+            <input type="text" placeholder="Search by title or author"/>
+
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid" >
-            <Book results= {this.state.results}/>
-            </ol>
+          <ol className="books-grid">
+            <li>
+              <div className="book">
+                <div className="book-top">
+                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                  <div className="book-shelf-changer">
+                    <select>
+                      <option value="none" disabled>Move to...</option>
+                      <option value="currentlyReading">Currently Reading</option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="book-title">To Kill a Mockingbird</div>
+                <div className="book-authors">Harper Lee</div>
+              </div>
+            </li>
+          </ol>
         </div>
       </div>
     )
   }
+
 }
 
 export default Search
